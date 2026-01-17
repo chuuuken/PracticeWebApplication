bcd2b2dfb57d0182ade0adaa525fc070e5e88125
ryo <nogami.fine@gmail.com>
2026-01-17 17:30:29 +0900

    add colors table record insert sql

---
+++ scheduleWebApp/db/colors_insert.sql
@@ -0,0 +1,8 @@
+insert into colors (color_name, color_code) values('青','#0000FF');
+insert into colors (color_name, color_code) values('緑','#00FF00');
+insert into colors (color_name, color_code) values('赤','#FF0000');
+insert into colors (color_name, color_code) values('黄','#FFFF00');
+insert into colors (color_name, color_code) values('ピンク','#FF00FF');
+insert into colors (color_name, color_code) values('水色','#00FFFF');
+insert into colors (color_name, color_code) values('白','#FFFFFF');
+insert into colors (color_name, color_code) values('黒','#000000');
